b5e72dc954cfd82d78ae6c13908f82d0c7d2bd27
ErnestoSSantos <ernesto.sales@gmail.com>
2023-10-16 10:06:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"aaa",
+    "title": "siu novo filme",
+    "description": "Blá blá blá blá",
+    "photoCover": "https://upload.wikimedia.org/wikipedia/pt/thumb/1/19/Iron_Man_3_poster.jpg/240px-Iron_Man_3_poster.jpg"
+  },
+  {
+    "id":"2",
+    "photo":"aaa2",
+    "title": "siu novo filme2",
+    "description": "Blá blá blá blá",
+    "photoCover": "https://upload.wikimedia.org/wikipedia/pt/thumb/1/19/Iron_Man_3_poster.jpg/240px-Iron_Man_3_poster.jpg"
+  }
+]
